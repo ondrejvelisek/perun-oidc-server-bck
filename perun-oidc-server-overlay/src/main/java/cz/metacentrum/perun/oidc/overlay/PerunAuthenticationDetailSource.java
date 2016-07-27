01e5eeeb6d07e55ea8e1a6c0debb1714d5bb57fe
ondrejvelisek <ondrejvelisek@gmail.com>
2016-07-27 14:08:55 +0200

    debug

--- perun-oidc-server-overlay/src/main/java/cz/metacentrum/perun/oidc/overlay/PerunAuthenticationDetailSource.java
+++ perun-oidc-server-overlay/src/main/java/cz/metacentrum/perun/oidc/overlay/PerunAuthenticationDetailSource.java
@@ -2,13 +2,20 @@ package cz.metacentrum.perun.oidc.overlay;
 
 import cz.metacentrum.perun.oidc.client.PerunPrincipal;
 import cz.metacentrum.perun.oidc.client.PerunUtils;
-import org.springframework.security.authentication.AuthenticationDetailsSource;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import cz.metacentrum.perun.oidc.client.User;
+import cz.metacentrum.perun.oidc.client.UsersManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,16 +26,30 @@ public class PerunAuthenticationDetailSource extends WebAuthenticationDetailsSou
 	@Override
 	public PerunWebAuthenticationDetails buildDetails(HttpServletRequest context) {
 
-		PerunWebAuthenticationDetails details = new PerunWebAuthenticationDetails(context);
-
 		PerunPrincipal pp = PerunUtils.parsePrincipal(context);
+		User user = UsersManager.getInstance().getUserByExtSourceNameAndExtLogin(pp.getExtSourceName(), pp.getUserExtSourceLogin());
+
+		if (user.getId() <= 0) {
+			throw new UsernameNotFoundException("Users id is " + user.getId());
+		}
+
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		// TODO remove
+		authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+		List<String> userIds = Arrays.asList(PerunUtils.getProperty("oidc.admins").split(","));
+		if (userIds.contains(String.valueOf(user.getId()))) {
+			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+
 		Map<String, Serializable> additionalInfo = new HashMap<>();
 		additionalInfo.put("extSourceName", pp.getExtSourceName());
 		additionalInfo.put("userExtSourceLogin", pp.getUserExtSourceLogin());
 		additionalInfo.put("extSourceType", pp.getExtSourceType());
 		additionalInfo.put("extSourceLoa", String.valueOf(pp.getExtSourceLoa()));
 
-		details.setAdditionalInfo(additionalInfo);
+		PerunWebAuthenticationDetails details = new PerunWebAuthenticationDetails(context, authorities, additionalInfo);
 
 		return details;
 	}
--- perun-oidc-server-overlay/src/main/java/cz/metacentrum/perun/oidc/overlay/PerunWebAuthenticationDetails.java
+++ perun-oidc-server-overlay/src/main/java/cz/metacentrum/perun/oidc/overlay/PerunWebAuthenticationDetails.java
@@ -1,33 +1,27 @@
 package cz.metacentrum.perun.oidc.overlay;
 
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedGrantedAuthoritiesWebAuthenticationDetails;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Map;
 
 /**
  * @author Ondrej Velisek <ondrejvelisek@gmail.com>
  */
-public class PerunWebAuthenticationDetails extends WebAuthenticationDetails {
+public class PerunWebAuthenticationDetails extends PreAuthenticatedGrantedAuthoritiesWebAuthenticationDetails {
 
 	private Map<String, Serializable> additionalInfo;
 
-	/**
-	 * Records the remote address and will also set the session Id if a session
-	 * already exists (it won't create one).
-	 *
-	 * @param request that the authentication request was received from
-	 */
-	public PerunWebAuthenticationDetails(HttpServletRequest request) {
-		super(request);
+	public PerunWebAuthenticationDetails(HttpServletRequest request, Collection<? extends GrantedAuthority> authorities,
+	                                     Map<String, Serializable> additionalInfo) {
+		super(request, authorities);
+		this.additionalInfo = additionalInfo;
 	}
 
 	public Map<String, Serializable> getAdditionalInfo() {
 		return additionalInfo;
-	}
-
-	public void setAdditionalInfo(Map<String, Serializable> additionalInfo) {
-		this.additionalInfo = additionalInfo;
 	}
 }
